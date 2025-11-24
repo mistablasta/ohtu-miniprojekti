df7cc498a718a74c17ceb7828b8b6fa54a87dfb8
Septicuss <vbalaban55@gmail.com>
2025-11-24 15:11:45 +0200

    feat: updated repository methods

--- src/repositories/entry_repository.py
+++ src/repositories/entry_repository.py
@@ -1,7 +1,114 @@
-from sqlalchemy import text
-from config import db
+import json
 
-from entities.entry import Entry
+from sqlalchemy import text
+
+from config import db
+from entities.entry import Entry, Type
+
+
+def create(key: str, type: Type, fields: dict):
+    """
+    Create a new entry
+    """
+    sql = text("""
+        INSERT INTO entries (key, type, fields)
+        VALUES (:key, :type, :fields)
+    """)
+    db.session.execute(sql, {
+        "key": key,
+        "type": type.name.lower(),
+        "fields": json.dumps(fields)
+    })
+    db.session.commit()
+
+def get(id: int) -> Entry:
+    """
+    Get an entry by its ID
+    """
+    sql = text("""
+        SELECT id, key, type, fields FROM entries
+        WHERE id = :id
+    """)
+    result = db.session.execute(sql, {"id": id})
+    return _parse_entry(result.fetchone())
+
+def get_all() -> list[Entry]:
+    """
+    Get all entries
+    """
+    sql = text("""SELECT * FROM entries""")
+    result = db.session.execute(sql)
+    return _parse_entries(result)
+
+def delete(id: int):
+    """
+    Delete an entry by its ID
+    """
+    sql = text("""DELETE FROM entries WHERE id = :id""")
+    db.session.execute(sql, {"id": id})
+
+def update(entry: Entry):
+    """
+    Update an entire entry
+    """
+    sql = text("""
+        UPDATE entries
+    """)
+    db.session.execute(sql, {
+        "key": entry.key,
+        "type": entry.type,
+        "fields": json.dumps(entry.fields)
+    })
+    db.session.commit()
+
+def search(query: str):
+    """
+    Search all entries matching the given string query.
+    This method will look for values in the fields or the entry key matching the query string.
+    """
+    sql = text("""
+       SELECT id, key, type, fields
+       FROM entries
+       WHERE EXISTS (
+           SELECT 1
+           FROM jsonb_each_text(fields) AS t(key, value)
+           WHERE value ILIKE :query
+       )
+       OR key ILIKE :query
+   """)
+
+    result = db.session.execute(sql, {"query": f"%{query}%"})
+    return _parse_entries(result.fetchall())
+
+def _parse_entries(result) -> list[Entry]:
+    """
+    Parse multiple entries at once from a result
+    """
+    return [_parse_entry(row) for row in result]
+
+def _parse_entry(result) -> Entry | None:
+    """
+    Parse a single entry from a result
+    """
+    if result is None:
+        return None
+
+    id = result[0]
+    key = result[1]
+    type = result[2]
+
+    if isinstance(type, str):
+        type_enum = Type[type.upper()]
+    else:
+        raise ValueError(f"Unknown entry type: {type}")
+
+    fields_json = result[3]
+    fields_dict = json.loads(fields_json) if isinstance(fields_json, str) else fields_json
+
+    return Entry(id=id, key=key, type=type_enum, fields=fields_dict)
+
+
+#-------------------------------------
 
 def get_entries():
     """Get entries from database"""
@@ -31,9 +138,9 @@ def add_entry(title, year, author, publisher, field):
     sql = text("""INSERT INTO entry (title, year, author, publisher, field)
                   VALUES (:title, :year, :author, :publisher, :field)""")
     db.session.execute(sql, {"title": title,
-                             "year": year, 
-                             "author": author , 
-                             "publisher": publisher, 
+                             "year": year,
+                             "author": author ,
+                             "publisher": publisher,
                              "field": field})
     db.session.commit()
 
