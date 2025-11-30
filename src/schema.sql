3147bbb167c4effe1980ade64edde7c407fc8295
Oskari Lainpelto <oskari.lainpelto@gmail.com>
2025-11-30 19:16:18 +0200

    feat: add tables for tags to schema

--- src/schema.sql
+++ src/schema.sql
@@ -19,3 +19,14 @@ CREATE TABLE entries (
     type TEXT NOT NULL,
     fields JSONB NOT NULL
 );
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE entry_tags (
+    entry_id INTEGER REFERENCES entries(id) ON DELETE CASCADE,
+    tag_id INTEGER REFERENCES tags(id) ON DELETE CASCADE,
+    PRIMARY KEY (entry_id, tag_id)
+);
