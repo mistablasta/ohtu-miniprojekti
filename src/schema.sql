a7250749eeff282e580f3516af94d5a9e099ef0c
Oskari Lainpelto <oskari.lainpelto@gmail.com>
2025-12-10 10:48:46 +0200

    refactor: remove obsolete entry table definition
    Replaced 'entry' table with 'entries' table definition.

--- src/schema.sql
+++ src/schema.sql
@@ -1,12 +1,3 @@
-CREATE TABLE entry (
-  id SERIAL PRIMARY KEY,
-  title TEXT,
-  year INTEGER,
-  author TEXT,
-  publisher TEXT,
-  field TEXT
-);
-
 CREATE TABLE entries (
     id SERIAL PRIMARY KEY,
     key TEXT NOT NULL,
